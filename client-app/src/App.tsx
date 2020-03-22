3ad79dca8eebdc7abccf59f665728d3ad6204adb
jasonportilla <jason.portilla85@gmail.com>
2020-03-22 08:49:27 -0400

    added client skeleton

---
+++ client-app/src/App.tsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
+import './App.css';
+
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+      .then((response) => {
+        this.setState({
+          values: response.data
+        });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <Header as='h2'>
+          <Icon name='users' />
+          <Header.Content>Reactivities</Header.Content>
+        </Header>
+        <List>
+          { this.state.values.map((value: any) => (
+              <List.Item key={value.id}>{value.name}</List.Item>
+          ))}
+        </List>
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+      </div>
+    );
+  }
+}
+
+export default App;
