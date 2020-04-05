e3277df496f03f03f5cfab4bc9466b3c72cb78af
jasonportilla <jason.portilla85@gmail.com>
2020-04-05 09:13:06 -0400

    added new routes to axios file

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -2,6 +2,7 @@ import { IActivity } from '../models/activity';
 import { history } from '../../';
 import { toast } from 'react-toastify';
 import axios, { AxiosResponse } from 'axios';
+import { IUser, IUserFormValues } from '../models/user';
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
@@ -42,6 +43,14 @@ const Activities = {
 	delete: (id: string) => requests.delete(`/activities/${id}`)
 };
 
+const User = {
+	current: (): Promise<IUser> => requests.get('/user'),
+	login: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/login`, user),
+	register: (user: IUserFormValues): Promise<IUser> => requests.post(`/user/register`, user),
+
+}
+
 export default {
-	Activities
+	Activities,
+	User
 };
