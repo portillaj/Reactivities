e31c24d08ac81dd74fbcfae96254595a31708542
jasonportilla <jason.portilla85@gmail.com>
2020-04-11 11:08:56 -0400

    removed attende endpoint

--- Application/Activities/Unattend.cs
+++ Application/Activities/Unattend.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using Application.Interfaces;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -25,7 +28,26 @@ namespace Application.Activities
         
                     public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
                     {
-                        // handler logic
+                        var activity = await _context.Activities.FindAsync(request.Id);
+
+                        if(activity == null) {
+                            throw new RestException(HttpStatusCode.NotFound, new {Activity = "Could not find activity"});
+                        }
+
+                        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == _userAccessor.GetCurrentUsername());
+
+                        var attendance = await _context.UserActivities.SingleOrDefaultAsync(
+                            x => x.ActivityId == activity.Id && x.AppUserId == user.Id);
+                        
+                        if(attendance == null) {
+                            return Unit.Value;
+                        }
+
+                        if(attendance.IsHost) {
+                            throw new RestException(HttpStatusCode.BadRequest, new { Attendance = "You cannot remove yourself as host"});
+                        }
+
+                        _context.UserActivities.Remove(attendance);
                         var success = await _context.SaveChangesAsync() > 0;
                         if(success) return Unit.Value;
                         throw new Exception("Problem saving changes");
---
+++ Infrastructure/Security/IsHostRequirement.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Infrastructure.Security
+{
+    public class IsHostRequirement : IAuthorizationRequirement
+    {
+    }
+
+    public class IsHostRequirementHandler : AuthorizationHandler<IsHostRequirement>
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly DataContext _context;
+        public IsHostRequirementHandler(IHttpContextAccessor httpContextAccessor, DataContext context)
+        {
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+       protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, IsHostRequirement requirement)
+        {
+            var currentUserName = _httpContextAccessor.HttpContext.User?.Claims?.SingleOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
+        
+            var activityId = Guid.Parse(_httpContextAccessor.HttpContext.Request.RouteValues.SingleOrDefault(x => x.Key == "id").Value.ToString());
+        
+            var activity = await _context.Activities.Include(x => x.UserActivities).ThenInclude(x => x.AppUser).SingleOrDefaultAsync(x => x.Id == activityId);
+        
+            var host = activity.UserActivities.FirstOrDefault(x => x.IsHost);
+        
+            if (host?.AppUser?.UserName == currentUserName)
+                context.Succeed(requirement);
+        
+            //return Task.CompletedTask;
+        }
+    }
+}
