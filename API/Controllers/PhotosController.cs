d45c448bf9635f528118e70c061960e58269d401
jasonportilla <jason.portilla85@gmail.com>
2020-04-22 19:36:07 -0400

    delete photo from cloudinary

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Application.Photos;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -11,6 +12,18 @@ namespace API.Controllers
         public async Task<ActionResult<Photo>> Add([FromForm]Add.Command command)
         {
             return await Mediator.Send(command);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Unit>> Delete(string id)
+        {
+            return await Mediator.Send(new Delete.Command{ Id = id});
+        }
+
+         [HttpPost("{id}/setmain")]
+        public async Task<ActionResult<Unit>> SetMain(string id)
+        {
+            return await Mediator.Send(new SetMain.Command{ Id = id});
         }
         
     }
